a4a20589db75604aad7e49ac579a4d12246475c5
hanyujie2002 <yujiehan2002@outlook.com>
2024-03-29 20:12:42 +0800

    Update conf.py

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -189,7 +189,7 @@ finally:
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
+language = 'zh-CN'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
